8a1d57a3fc268fcac5cea740c09318e5521f2751
Justin Hamerly <suvimountain@icloud.com>
2023-09-27 15:28:34 -0700

    added methods to return strings describing object fields

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 class GameObject:
+
 	def __init__(self, name, appearance, feel, smell):
 		self.name = name
 		self.appearance = appearance
 		self.feel = feel
 		self.smell = smell
+
+	def look(self):
+		return f"You look at the {self.name}. {self.appearance}\n"
+	
+	def touch(self):
+		return f"You touch the {self.name}. {self.feel}\n"
+	
+	def sniff(self):
+		return f"You sniff the {self.name}. {self.smell}\n"
